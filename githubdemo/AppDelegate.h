2f1330845e34e36743ebe05e9a82ede3eb6f8d33
Sethu <Sethu@sethus-imac.local>
2016-04-02 09:17:41 +0530

    Initial Commit

---
+++ githubdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubdemo
+//
+//  Created by Sethu on 02/04/16.
+//  Copyright © 2016 kaizen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubdemo
+//
+//  Created by Sethu on 02/04/16.
+//  Copyright © 2016 kaizen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
